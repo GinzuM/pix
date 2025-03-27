b7862338a9a811870d539a5cd993ed26052bd2c8
GinzuM <87874786+GinzuM@users.noreply.github.com>
2025-03-27 18:44:55 -0300

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -6,31 +6,38 @@ export default async function handler(req, res) {
 
         try {
             console.log(`[${new Date().toISOString()}] Iniciando a requisição para obter o token...`);
-            
+
+            // Constrói o corpo da requisição
+            const body = new URLSearchParams({
+                'grant_type': 'client_credentials',
+                'client_id': clientId,
+                'client_secret': clientSecret,
+                'scope': 'account:read wallet:read webhooks',
+            });
+
+            console.log(`[${new Date().toISOString()}] Corpo da requisição: ${body.toString()}`);
+
+            // Envia a requisição
             const response = await fetch(tokenUrl, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
-                body: new URLSearchParams({
-                    'grant_type': 'client_credentials',
-                    'client_id': clientId,
-                    'client_secret': clientSecret,
-                    'scope': 'account:read wallet:read webhooks',
-                }),
+                body: body,
             });
 
-            console.log(`[${new Date().toISOString()}] Status da resposta: ${response.status} - ${response.statusText}`);
-            
+            console.log(`[${new Date().toISOString()}] Status da resposta da API: ${response.status} - ${response.statusText}`);
+
             if (!response.ok) {
-                const errorMessage = `Erro ao obter o token: ${response.status} - ${response.statusText}`;
-                console.error(`[${new Date().toISOString()}] ${errorMessage}`);
-                throw new Error(errorMessage);
+                const errorText = await response.text(); // Adicional: obtenha detalhes do erro
+                console.error(`[${new Date().toISOString()}] Erro na resposta da API: ${errorText}`);
+                throw new Error(`Erro ao obter o token: ${response.status} - ${response.statusText}`);
             }
 
+            // Processa a resposta JSON
             const data = await response.json();
             console.log(`[${new Date().toISOString()}] Token gerado com sucesso: ${data.access_token}`);
-            
+
             res.status(200).json({ token: data.access_token });
         } catch (error) {
             console.error(`[${new Date().toISOString()}] Erro ao obter o token: ${error.message}`);
